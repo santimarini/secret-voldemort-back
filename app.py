1ee1d05cacbbec8202c6573c50c32eeaafea808d
Joaquin <jotacentini@gmail.com>
2020-10-21 20:54:31 -0300

    ready to merge

--- app.py
+++ app.py
@@ -9,15 +9,9 @@ async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user = get_user_by_email(form_data.username)
     if user is None:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-    hashed_password = fake_hash_password(form_data.password)
+    hashed_password = hash_password(form_data.password)
     if not hashed_password == user.password:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-
+    # return token to identify a specific user, it'll be the user's email for simplicity
     return {"access_token": user.email_address, "token_type": "bearer"}
 
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    if current_user.verified == 0:
-        raise HTTPException(status_code=400, detail="Email no verified")
-    return current_user.email_address
--- loginfunctions.py
+++ loginfunctions.py
@@ -4,22 +4,6 @@ from database import *
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-def fake_hash_password(password: str):
+def hash_password(password: str):
     return "fakehashed" + password
-
-## decode toke
-def fake_decode_token(token):
-    user = get_user_by_email(token)
-    if user is not None:
-        return user
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    user = fake_decode_token(token)
-    if not user:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid authentication credentials bla",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    return user
 
