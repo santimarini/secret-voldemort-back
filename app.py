3ba2d6e80957782d397289dd1a617ef984876d26
Joaquin <jotacentini@gmail.com>
2020-10-21 16:35:43 -0300

    endpoint integrated with the DB

--- app.py
+++ app.py
@@ -1,27 +1,23 @@
 from fastapi import FastAPI
-## from fastapi_users.authentication import JWTAuthentication
 from loginfunctions import *
-## from pony import
+
 
 app = FastAPI()
 
 @app.post("/token")
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
-    user_dict = fake_users_db.get(form_data.username)
-    print(form_data.username)
-    if not user_dict:
+    user = get_user_by_email(form_data.username)
+    if user is None:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-    user = UserInDB(**user_dict)
     hashed_password = fake_hash_password(form_data.password)
-    if not hashed_password == user.hashed_password:
+    if not hashed_password == user.password:
         raise HTTPException(status_code=400, detail="Incorrect username or password")
-    return {"access_token": user.email, "token_type": "bearer"}
 
-## get_current_verified_user podemos verificar si tiene el email verificado
+    return {"access_token": user.email_address, "token_type": "bearer"}
+
+
 @app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_verified_user)):
-    return current_user
-
-#@app.post("/user/validateemail")
-#async def validate_email(recibo un token conA)
-#    deberia modificar la base de datos
+async def read_users_me(current_user: User = Depends(get_current_user)):
+    if current_user.verified == 0:
+        raise HTTPException(status_code=400, detail="Email no verified")
+    return current_user.email_address
