dedb5da8fcfe733df32562c7ac32b333e7d9ded3
valentin <ValenVilarDav@bitbucket.org>
2020-10-21 20:51:14 -0300

    added comments

--- database/database.py
+++ database/database.py
@@ -5,7 +5,7 @@ db = pony.orm.Database()
 # db.bind(provider='mysql', host='localhost', user='valentin', passwd='valentin', db='ponytest')
 db.bind(provider='sqlite', filename='database.sqlite', create_db=True)
 
-
+#Users table
 class User(db.Entity):
     name = pony.orm.Required(str)
     email_address = pony.orm.Required(str, unique=True) #SK
@@ -15,22 +15,20 @@ class User(db.Entity):
 
 db.generate_mapping(create_tables=True)
 
-# no se si la definicion de funciones deberia ir dentro de la clase o no pero weno
+#Database functions
+
+#Creates a new user
 @pony.orm.db_session
 def new_user(name, email_address, password, photo):
     User(name=name, email_address=email_address, password=password,
          photo=photo, verified=True)
 
-@pony.orm.db_session
-def login(email_address):
-    user1 = User.get(email_address=email_address)
-    if user1 is not None:
-        user1.is_logged = True
-
+#given a email, returns the associate email
 @pony.orm.db_session
 def get_user_by_email(email_address):
     return(User.get(email_address=email_address))
 
+#verify if certain email exists
 @pony.orm.db_session
 def email_exists(email_address):
     return(User.get(email_address=email_address) is not None)
